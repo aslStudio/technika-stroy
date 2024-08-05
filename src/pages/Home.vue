edcb6fdc523e60bc5eebc364d07e1b22c5d8a3e3
Vlad AStahov <vlad-astahov@mail.ru>
2024-08-06 02:40:01 +0300

    init template

---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+iframe(
+    :class="$style.root"
+    src="https://dev.technikastroy.ru/client"
+)
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue"; 
+
+export default defineComponent({
+    name: 'Home',
+    setup() {
+        function onClick() {
+            console.log('CLICK')
+        }
+
+        onMounted(() => {
+            window.addEventListener('message', e => {
+                console.log('event: ',e, )
+            })
+        })
+
+        return {
+            onClick
+        }
+    }
+})
+</script>
+
+<style lang="scss" module>
+.root {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
